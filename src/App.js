794f71b8ded7f16a87580e5f76929909bd72d646
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-03-30 12:23:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component{
       textoFrase: ''
     }
 
-    this.frases = ['Siga os bons e aprenda com eles.','O bom-senso vale mais do que muito conhecimento.',
+    this.frases = ['Siga os  bons e aprenda com eles.','O bom-senso vale mais do que muito conhecimento.',
     'O riso é a menor distância entre duas pessoas.','Deixe de lado as preocupações e seja feliz.',
     'Realize o óbvio, pense no improvável e conquiste o impossível.','Acredite em milagres, mas não dependa deles.',
     'A maior barreira para o sucesso é o medo do fracasso.']
